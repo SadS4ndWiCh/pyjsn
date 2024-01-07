08fc7bc9d0632a7975cdd369bce223d6adf8149c
sads4ndwich <caiocamargo007@gmail.com>
2024-01-07 20:31:29 -0300

    feat: added tokens

---
+++ src/tokens.py
@@ -0,0 +1,29 @@
+from enum import Enum
+from dataclasses import dataclass
+
+class Tokens(Enum):
+	INVALID = "INVALID"
+	EOF     = "EOF"
+	
+	OPBRACE   = "OPBRACE"
+	CLBRACE   = "CLBRACE"
+	OPBRACKET = "OPBRACKET"
+	CLBRACKET = "CLBRACKET"
+	COMMA     = "COMMA"
+	COLON     = "COLON"
+	
+	
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	STRING = "STRING"
+	NUMBER = "NUMBER"
+
+keywords = {
+	"true": Tokens.TRUE,
+	"false": Tokens.FALSE
+}
+
+@dataclass
+class Token:
+	type: Tokens
+	literal: str
