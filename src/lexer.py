5943281016087ae66ad83c89f305584195c8430d
sads4ndwich <caiocamargo007@gmail.com>
2024-01-07 22:16:10 -0300

    fix(indent): fix file indentation

--- src/lexer.py
+++ src/lexer.py
@@ -1,105 +1,105 @@
 from src.tokens import Tokens, Token, lookupIdent
 
 class Lexer:
-	tokens = {
-		"{": Token(Tokens.OPBRACE, "{"),
-		"}": Token(Tokens.CLBRACE, "}"),
-		"[": Token(Tokens.OPBRACKET, "["),
-		"]": Token(Tokens.CLBRACKET, "]"),
-		":": Token(Tokens.COLON, ":"),
-		",": Token(Tokens.COMMA, ",")
-	}
-	
-	def __init__(self, input):
-		self.input = input
-		self.pos = 0
-		self.nextPos = 0
-		self.ch = None
-		
-		self._readChar()
-	
-	def _readChar(self):
-		if self.nextPos >= len(self.input):
-			self.ch = None
-			return
-		
-		self.pos = self.nextPos
-		self.ch = self.input[self.pos]
-		self.nextPos += 1
-	
-	def _isText(self, ch):
-		return ch != '"' and ch != "\n"
-	
-	def _isIdent(self, ch):
-		return ch in "abcdefghijklmniopqrstuvwxyz0123456789"
-	
-	def _isDigit(self, ch):
-		return ch in "0123456789."
-	
-	def _getText(self):
-		self._readChar()
-		pos = self.pos
-		
-		while self._isText(self.ch):
-			self._readChar()
-		
-		text = self.input[pos:self.pos]
-		self._readChar()
-		
-		return text
-	
-	def _getIdent(self):
-		pos = self.pos
-		
-		while self._isIdent(self.ch):
-			self._readChar()
-		
-		ident = self.input[pos:self.pos]
-		
-		return ident
-	
-	def _getDigit(self):
-		pos = self.pos
-		
-		while self._isDigit(self.ch):
-			self._readChar()
-		
-		digit = self.input[pos:self.pos]
-		
-		return digit
-	
-	def _eatWhitespace(self):
-		while self.ch in [" ", "\n", "\t", "\r"]:
-			self._readChar()
-	
-	def nextToken(self):
-		self._eatWhitespace()
-		
-		ch = self.ch
-		tok = Token(Tokens.INVALID, None)
-		
-		if ch in Lexer.tokens:
-			tok = Lexer.tokens[ch]
-		
-		elif ch == '"':
-			tok = Token(Tokens.STRING, self._getText())
-			return tok
-		
-		elif ( not ch is None ) and self._isDigit(self.ch):
-			digit = self._getDigit()
-			
-			tok = Token(Tokens.NUMBER, digit)
-			return tok
-		
-		elif ( not ch is None ) and self._isText(self.ch):
-			ident = self._getIdent()
-			
-			tok = Token(lookupIdent(ident), ident)
-			return tok
-			
-		elif ch is None:
-			tok = Token(Tokens.EOF, "")
-		
-		self._readChar()
-		return tok
+    tokens = {
+        "{": Token(Tokens.OPBRACE, "{"),
+        "}": Token(Tokens.CLBRACE, "}"),
+        "[": Token(Tokens.OPBRACKET, "["),
+        "]": Token(Tokens.CLBRACKET, "]"),
+        ":": Token(Tokens.COLON, ":"),
+        ",": Token(Tokens.COMMA, ",")
+    }
+
+    def __init__(self, input):
+        self.input = input
+        self.pos = 0
+        self.nextPos = 0
+        self.ch = None
+
+        self._readChar()
+
+    def _readChar(self):
+        if self.nextPos >= len(self.input):
+            self.ch = None
+            return
+
+        self.pos = self.nextPos
+        self.ch = self.input[self.pos]
+        self.nextPos += 1
+
+    def _isText(self, ch):
+        return ch != '"' and ch != "\n"
+
+    def _isIdent(self, ch):
+        return ch in "abcdefghijklmniopqrstuvwxyz0123456789"
+
+    def _isDigit(self, ch):
+        return ch in "0123456789."
+
+    def _getText(self):
+        self._readChar()
+        pos = self.pos
+
+        while self._isText(self.ch):
+            self._readChar()
+
+        text = self.input[pos:self.pos]
+        self._readChar()
+
+        return text
+
+    def _getIdent(self):
+        pos = self.pos
+
+        while self._isIdent(self.ch):
+            self._readChar()
+
+        ident = self.input[pos:self.pos]
+
+        return ident
+
+    def _getDigit(self):
+        pos = self.pos
+
+        while self._isDigit(self.ch):
+            self._readChar()
+
+        digit = self.input[pos:self.pos]
+
+        return digit
+
+    def _eatWhitespace(self):
+        while self.ch in [" ", "\n", "\t", "\r"]:
+            self._readChar()
+
+    def nextToken(self):
+        self._eatWhitespace()
+
+        ch = self.ch
+        tok = Token(Tokens.INVALID, None)
+
+        if ch in Lexer.tokens:
+            tok = Lexer.tokens[ch]
+
+        elif ch == '"':
+            tok = Token(Tokens.STRING, self._getText())
+            return tok
+
+        elif ( not ch is None ) and self._isDigit(self.ch):
+            digit = self._getDigit()
+
+            tok = Token(Tokens.NUMBER, digit)
+            return tok
+
+        elif ( not ch is None ) and self._isText(self.ch):
+            ident = self._getIdent()
+
+            tok = Token(lookupIdent(ident), ident)
+            return tok
+
+        elif ch is None:
+            tok = Token(Tokens.EOF, "")
+
+        self._readChar()
+        return tok
 
--- src/tokens.py
+++ src/tokens.py
@@ -2,36 +2,36 @@ from enum import Enum
 from dataclasses import dataclass
 
 class Tokens(Enum):
-	INVALID = "INVALID"
-	EOF     = "EOF"
-	
-	OPBRACE   = "OPBRACE"
-	CLBRACE   = "CLBRACE"
-	OPBRACKET = "OPBRACKET"
-	CLBRACKET = "CLBRACKET"
-	COMMA     = "COMMA"
-	COLON     = "COLON"
-	
-	
-	TRUE   = "TRUE"
-	FALSE  = "FALSE"
-	STRING = "STRING"
-	NUMBER = "NUMBER"
+    INVALID = "INVALID"
+    EOF     = "EOF"
+
+    OPBRACE   = "OPBRACE"
+    CLBRACE   = "CLBRACE"
+    OPBRACKET = "OPBRACKET"
+    CLBRACKET = "CLBRACKET"
+    COMMA     = "COMMA"
+    COLON     = "COLON"
+
+
+    TRUE   = "TRUE"
+    FALSE  = "FALSE"
+    STRING = "STRING"
+    NUMBER = "NUMBER"
 
 keywords = {
-	"true": Tokens.TRUE,
-	"false": Tokens.FALSE
+    "true": Tokens.TRUE,
+    "false": Tokens.FALSE
 }
 
 
 def lookupIdent(ident):
-        if ident in keywords:
-                return keywords[ident]
-        
-        return ident
+    if ident in keywords:
+        return keywords[ident]
+
+    return ident
 
 
 @dataclass
 class Token:
-	type: Tokens
-	literal: str
+    type: Tokens
+    literal: str
